8126e2007be567711f2f437ac429825de6c42efd
Jose <joisro@uchicago.edu>
2018-04-04 14:49:20 -0500

    update example1

--- Code/Test_Examples/Example1.m
+++ Code/Test_Examples/Example1.m
@@ -2,10 +2,12 @@ k=2 % Number of parameters
 theN=[3,3] %Dimension of MEP
 assert(length(theN)==k)  
 %%
-A=cell(theK,theK+1)
+A=cell(k,k+1)
 for i = 1:k
   for j=1:k+1
     A{i,j}=randn(theN(i),theN(i));
   end    
 end
-FiberHomotopy2(A,varargin)
+A
+%%
+FiberHomotopy2(A)

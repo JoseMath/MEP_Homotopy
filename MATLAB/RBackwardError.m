b19ca16ba89b42265e5b6c6698d431fdc2ea4815
jaydu1 <413075930@qq.com>
2020-10-20 22:57:36 -0500

    Update RBackwardError.m

--- MATLAB/RBackwardError.m
+++ MATLAB/RBackwardError.m
@@ -5,7 +5,7 @@ function [MaxRelError,RelError,Position,ri,thetai] = RBackwardError(EigenValue,E
     %    A           : k by k+1 cell, matrix coefficients
     % Returns:
     %    MaxRelError : n_sols by 1 matrix, maximum backward errors among k equations
-    %    RelError    : n_sols by 1 matrix, backward errors
+    %    RelError    : n_sols by k matrix, backward errors
     %    Position    : n_sols by 1 matrix, where the MaxRelError obtains
     %    ri          : n_sols by k matrix, the l2 norm of the residuals
     %    thetai      : n_sols by k matrix, ||A_{i0}||+sum_k |lambda_j|*||A_{ij}||
